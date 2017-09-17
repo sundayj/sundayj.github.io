60c67ab675991a32bdc495fa155dbacdeefe0ef5
sundayj <justin.sunday@live.com>
2017-09-16 23:07:58 -0400

    Updates
    Changed fonts, added accordions, fixed table, added links, updated some styles, and added a contact me section.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,41 @@ function maindropdown() {
         respNav.className += "responsive";
     } else {
         respNav.className = "mainnav";
-    }   
+    }
 }
 
 document.getElementById("hamburger").addEventListener("click", maindropdown);
+
+
+//Accordion for Experience Section
+var acc = document.getElementsByClassName("position");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    }
+}
+
+
+//Accordion for Education Section
+var acc = document.getElementsByClassName("school");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    }
+}
